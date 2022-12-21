69c9a411aba6c33a0e894671732ed7e3890171be
Konstantin <83224669+Htomsik@users.noreply.github.com>
2022-12-22 01:06:08 +0300

    add HalfSizeConverter.cs

---
+++ AvaloniaUIClient/Converters/HalfSizeConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace AvaloniaUIClient.Converters;
+
+internal sealed class HalfSizeConverter : IValueConverter
+{
+    
+    public static readonly HalfSizeConverter Instance = new();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double && !double.IsNaN((double)value))
+            return (double)value / 2;
+        
+        return value;
+    }
+
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+  
+}
