b5d6c14c99aabbcc0ea445a9576d475f5e322f9f
Konstantin <83224669+Htomsik@users.noreply.github.com>
2023-01-01 01:02:13 +0300

    add settings command into MainMenuVmd.cs

--- Core/VMD/MainMenuVmd.cs
+++ Core/VMD/MainMenuVmd.cs
@@ -1,12 +1,19 @@
+using Core.Infrastructure.Hosting;
+using Core.Infrastructure.Services;
 using Core.VMD.Base;
+using Core.VMD.TitleVmds;
+using Microsoft.Extensions.DependencyInjection;
+using ReactiveUI;
 
 
 namespace Core.VMD;
 
 public class MainMenuVmd : BaseVmd
 {
+    private IReactiveCommand OpenSettings { get; }
     public MainMenuVmd()
     {
-        
+        OpenSettings = ReactiveCommand.Create(
+            ()=> HostWorker.Services.GetService<BaseVmdNavigationService<ITitleVmd>>()!.Navigate(typeof(SettingsVmd)));
     }
 }
