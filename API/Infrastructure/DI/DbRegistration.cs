9c82946dfc4eaaffb02ce24fada7fa31df77d0fe
Konstantin <83224669+Htomsik@users.noreply.github.com>
2023-01-28 23:12:30 +0300

    Add API DB infrastructure

---
+++ API/Infrastructure/DI/DbRegistration.cs
@@ -0,0 +1,34 @@
+using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Infrastructure.DI;
+
+internal static partial class DiRegistration
+{
+    private const string ConnectionString = "Data Source=";
+
+    public static IServiceCollection AddDataBase(this IServiceCollection services, IConfiguration configuration) => 
+        services.AddDbContext<DataDb>(db =>
+        {
+            var type = configuration["Database:Type"];
+            
+            switch (type)
+            {
+                case "MSSQL":
+                    db.UseSqlServer($"{ConnectionString}{configuration.GetConnectionString(type)}", 
+                        x => x.MigrationsAssembly(nameof(DAL) + ".SqlServer"));
+                    break;
+                
+                case "SQLLite":
+                    db.UseSqlite( $"{ConnectionString}{configuration.GetConnectionString(type)}", 
+                        x=>x.MigrationsAssembly(nameof(DAL) + ".SqlLite"));
+                    break;
+
+                case null: 
+                    throw new ArgumentNullException($"{nameof(type)} can't be null. Check configuration file/");
+                
+                default: 
+                    throw new NotSupportedException($"DataBase {type} doesn't supported");
+            }
+        });
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using API.Infrastructure.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// DI registration
+builder.Services.AddDataBase(builder.Configuration);
 
 var app = builder.Build();
 
