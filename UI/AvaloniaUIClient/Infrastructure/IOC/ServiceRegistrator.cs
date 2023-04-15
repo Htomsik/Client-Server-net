198b86f4459b7ece02adc27b9227b70380ca53c7
Konstantin <83224669+Htomsik@users.noreply.github.com>
2023-04-15 20:57:19 +0300

    Fetch: Add UiThread operations infrastructure

---
+++ UI/AvaloniaUIClient/Infrastructure/IOC/ServiceRegistrator.cs
@@ -0,0 +1,12 @@
+﻿using AvaloniaUIClient.Infrastructure.Services.Other;
+using Core.Infrastructure.Services.Other;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AvaloniaUIClient.Infrastructure.IOC;
+
+internal static partial class IocRegistrator
+{
+    public static IServiceCollection ServiceRegistrator(this IServiceCollection services) =>
+        services
+            .AddTransient<IUiThreadOperation,UiThreadOperation>();
+}
